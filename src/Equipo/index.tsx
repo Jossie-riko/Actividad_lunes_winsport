f3974f946d677a5b03c458f72bcb6a73743538de
Jossie-riko <jossie.ricob@uniagustiniana.edu.co>
2026-04-13 21:05:11 +0000

    13/04/26

--- src/Equipo/index.tsx
+++ src/Equipo/index.tsx
@@ -1,10 +1,55 @@
 import "./style.css";
-function Equipo (){
-    return ( 
-        <>
-        <p>Equipo</p>
-        
-    </>
-    )
+import { useParams } from 'react-router';
+import { useEffect, useState } from "react";
+
+interface TeamData {
+  team: {
+    name: string;
+    info: {
+      city: string;
+      founded: string;
+      stadium: string;
+      president: string;
+      last_title: string;
+    };
+    ranking: {
+      position: string;
+      competition: string;
+    };
+    social: {
+      facebook: string;
+      instagram: string;
+      x: string;
+    };
+    links: {
+      store: string;
+      tickets: string;
+    };
+  };
 }
+function Equipo() {
+    const { equipo } = useParams<{ equipo: string }>();
+    const [data, setData]= useState<TeamData | null>(null);
+      useEffect(() => {
+        if (!equipo) return;
+        const fetchData = async () => {
+          try {
+            const res = await fetch('https://raw.githubusercontent.com/sdtibata/dataliga/refs/heads/main/posiciones.json');
+
+            const data = await res.json();
+    
+            setData(data);
+          } catch (error) {
+            console.error('Error cargando datos:', error)
+          }
+        }
+          fetchData()
+}, [equipo]);
+  return (
+    <>
+      <p>{equipo}</p>
+    </>
+  )
+}
+
 export default Equipo
